f08a88093a8f2cb5483e2742ea9648ceffa3efb4
keki030215 <keliuhan215@gmail.com>
2023-03-01 16:02:46 -0800

    folder added

---
+++ ps6-shinyweb/server.R
@@ -0,0 +1,8 @@
+library(shiny)
+library(tidyverse)
+library(ggplot2)
+
+server <- function(input, output) {
+  # define output reactive expressions
+}
+shinyServer(server)
---
+++ ps6-shinyweb/ui.R
@@ -0,0 +1,8 @@
+library(shiny)
+library(tidyverse)
+library(ggplot2)
+
+ui <- fluidPage(
+  # define widgets
+)
+shinyUI(ui)

03e1b79c627bca89d4b7f271721eaf5179f68003
keki030215 <keliuhan215@gmail.com>
2023-03-03 17:55:50 -0800

    code modified

--- ps6-shinyweb/server.R
+++ ps6-shinyweb/server.R
@@ -1,8 +1,85 @@
 library(shiny)
 library(tidyverse)
 library(ggplot2)
+library(plotly)
+
+setwd("~/Documents/info201wi23/assignments/ps6-shinywebapp/ps6-shinyweb/")
+uah <- read_delim("UAH-lower-troposphere-long.csv.bz2")
 
 server <- function(input, output) {
-  # define output reactive expressions
+  output$sample_table <- renderTable({
+    uah_sample <- uah %>% 
+      mutate(time = year + month/100*(100/12)) %>% 
+      sample_n(5)
+    print(uah_sample)
+  })
+  output$plot <- renderPlotly({
+    uah_plot <- uah %>% 
+      filter(region %in% input$select_region) %>% 
+      mutate(time = year + month/100*(100/12)) 
+  p <- ggplot(uah_plot, aes(time, temp, col = region)) +
+              geom_point(size = 0.5) +
+              labs(x = "Year", y = "Temperature", col = "Region") +
+              xlim(input$year_range[1], input$year_range[2])
+  ggplotly(p, height = 500, width = 700)
+  })
+  output$plot_text1 <- renderText({
+    paste("Time period is from", input$year_range[1],
+        "to", input$year_range[2])
+  })
+  output$plot_text2 <- renderText({
+    paste("You have selected", input$select_region)
+  })
+  output$table <-renderDataTable({
+    var <- input$variable
+    if(var == "year"){
+      uah_table <- uah %>% 
+        group_by(year) %>% 
+        reframe(ave_temp = round(mean(temp), 4))
+    }
+    else{
+      if(var == "month"){
+        uah_table <- uah %>% 
+          group_by(year, month) %>% 
+          reframe(ave_temp = round(mean(temp), 4))
+      }
+      else{
+        if(var == "region"){
+          uah_table <- uah %>% 
+            group_by(region) %>% 
+            reframe(ave_temp = round(mean(temp), 4))
+        }
+      }
+    }
+  })
+  output$table_text <- renderText({
+    var <- input$variable
+    if(var == "year"){
+      uah_table <- uah %>% 
+        group_by(year) %>% 
+        reframe(ave_temp = round(mean(temp), 4))
+      paste("The table has", n_distinct(uah$year), "years' data, and 
+             the temperature range is from", min(uah_table$ave_temp),
+            "to", max(uah_table$ave_temp), ".\n")
+    }else{
+      if(var == "month"){
+        uah_table <- uah %>% 
+          group_by(year, month) %>% 
+          reframe(ave_temp = round(mean(temp), 4))
+        paste("The table has", nrow(uah_table), "months' data, and 
+             the temperature range is from", min(uah_table$ave_temp),
+              "to", max(uah_table$ave_temp), ".\n")
+      }else{
+        if(var == "region"){
+          uah_table <- uah %>% 
+            group_by(region) %>% 
+            reframe(ave_temp = round(mean(temp), 4))
+          paste("The table has", nrow(uah_table), "regions' data, and 
+             the temperature range is from", min(uah_table$ave_temp),
+                "to", max(uah_table$ave_temp), ".\n")
+        }
+      }
+    }
+  })
 }
 shinyServer(server)
--- ps6-shinyweb/ui.R
+++ ps6-shinyweb/ui.R
@@ -1,8 +1,74 @@
 library(shiny)
 library(tidyverse)
 library(ggplot2)
+library(plotly)
+
+setwd("~/Documents/info201wi23/assignments/ps6-shinywebapp/ps6-shinyweb/")
+uah <- read_delim("UAH-lower-troposphere-long.csv.bz2")
 
 ui <- fluidPage(
-  # define widgets
-)
+  titlePanel("UAH Lower Troposphere"), 
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Introduction",
+                 h3("Brief Introfuction of The Dataset"),
+                 p(em("The dataset is from UAH.")),
+                 p("It includes four main variables:
+                   year, month, temperature, and region."),
+                 p("During data processing, another column of the table
+                   called",
+                   strong("time"), 
+                   "is converted by mixing the year and month data."), 
+                 p("The dataset measures the", 
+                   strong("temperature deviation"), em("(deg C)"), 
+                 "from", strong("1978-2023"), "average that are aggregated 
+                   over a number of", strong("different regions.")),
+                 p(em("Here is a small random sample set of the data.\n")),
+                 tableOutput("sample_table")
+                 ),
+        tabPanel("Plot",
+                 sidebarLayout(
+                   sidebarPanel(
+                     sliderInput(
+                       "year_range", "Select the range of year",
+                       min(uah$year), max(uah$year), 
+                       c(min(uah$year), max(uah$year))
+                     ),
+                     checkboxGroupInput(
+                       "select_region", "Select region(s)",
+                       choices = c( "globe", "globe_land", "globe_ocean", "nh", "nh_land",   
+                                    "nh_ocean", "sh", "sh_land", "sh_ocean", "trpcs",       
+                                    "trpcs_land", "trpcs_ocean", "noext", "noext_land", "noext_ocean",
+                                    "soext", "soext_land", "soext_ocean", "nopol", "nopol_land",  
+                                    "nopol_ocean", "sopol", "sopol_land", "sopol_ocean", "usa48",      
+                                    "usa49", "aust"),
+                       selected = "globe")
+                     ),
+                   mainPanel(
+                     textOutput("plot_text1"),
+                     textOutput("plot_text2"),
+                     plotlyOutput("plot")
+                   )
+                  )
+                 ),
+        tabPanel("Table",
+                 sidebarLayout(
+                   sidebarPanel(
+                     p("This table display average temperature over
+                       the variable you choose."),
+                     p("Here is a line summarize the data 
+                       based on your selection."),
+                     radioButtons("variable", 
+                                 "Select average temperature depends on:",
+                                 c("year", "month", "region"), "year")
+                   ),
+                   mainPanel(
+                     textOutput("table_text"),
+                     dataTableOutput("table")
+                   )
+                 )
+        ),
+      )
+    )
+  )
 shinyUI(ui)
